c523904e7e1605ea40c61a2a0e53390e7ab87cf7
cman131 <cgw5994@rit.edu>
2014-11-10 13:33:42 -0500

    added ALL of the src

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,6 +1,7 @@
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.Console;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 
@@ -9,15 +10,37 @@ import java.util.regex.Pattern;
  * /
  */
 
+/* We don't need an entire class implementation, just seemed like a lot
+ * of stuff to put in one main() method.
+ */
 public class CGrep {
 
-	/**
-	 * @param args
-	 * @throws FileNotFoundException 
-	 */
-	public static void main(String[] args) throws FileNotFoundException {
-		Pattern pattern = Pattern.compile(args[0]);
-		FileInputStream file = new FileInputStream(new File(args[1]));
-		
-	}
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        //Check if no args plz
+        if (args.length <= 0) {
+            System.out.println("Please argumentize.");
+            System.exit(1);
+        }
+
+        ArrayList<String> files = new ArrayList<String>();
+        Pattern pattern = Pattern.compile(args[0]);
+
+        if (args.length == 1) {
+            String input;
+            Console console = System.console();
+            input = console.readLine("Enter input:");
+        } else {
+            files.addAll(Arrays.asList(args).subList(1, args.length));
+        }
+
+
+        // Create the Grep Executor
+        GrepExecutor ge = new GrepExecutor();
+        ge.beginSearch(files, pattern);
+
+    }
 }
---
+++ src/Found.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ * Created by kocsen on 10/29/14.
+ * <p/>
+ * Class used to encapsulate the results of the Grepper
+ */
+public class Found {
+    private final String filename;
+    private ArrayList<String> matchingLines;
+
+
+    /**
+     * Data container for whatever the Grepper found in the file.
+     *
+     * @param filename      - The file name that the Grepper searched through
+     * @param matchingLines - Array
+     *                      Formatted: ["$line# $actualline"...]
+     */
+    public Found(String filename, ArrayList<String> matchingLines) {
+        this.filename = filename;
+        this.matchingLines = matchingLines;
+    }
+
+    public ArrayList<String> getMatchingLines() {
+        return this.matchingLines;
+    }
+    
+    @Override
+    public String toString() {
+    	// A nice and fancy print out of the results for another class to print
+    	StringBuilder retString = new StringBuilder();
+    	retString.append("\nFile: "+filename+"\n");
+    	retString.append("----------------\n");
+    	retString.append("Matching Lines:\n");
+    	
+    	// Totally fancy list of matching lines
+    	for(String str : matchingLines){
+    		retString.append(str+"\n");
+    	}
+    	return retString.toString();
+    }
+
+}
