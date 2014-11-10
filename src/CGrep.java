a49b2dc8617263e2330fef696590cd8e6971d834
cman131 <cgw5994@rit.edu>
2014-11-10 13:31:57 -0500

    added grep src from old project

---
+++ src/CGrep.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.regex.Pattern;
+
+
+/**
+ * Main CGREP class for concurrent lock
+ * /
+ */
+
+public class CGrep {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		Pattern pattern = Pattern.compile(args[0]);
+		FileInputStream file = new FileInputStream(new File(args[1]));
+		
+	}
+}
