1400ef5ea5207430b1443dfe025e7aacdabba146
kocsen <kocsenc@gmail.com>
2014-11-16 14:08:56 -0500

    - configure message implemented

--- src/Actors/ConfigureMessage.java
+++ src/Actors/ConfigureMessage.java
@@ -1,9 +1,17 @@
 package Actors;
 
+import akka.actor.ActorRef;
+
 /**
  * Created by kocsen on 11/14/14.
  */
 public class ConfigureMessage {
 
+    public final String filename;
+    public final ActorRef collectionActor;
 
+    public ConfigureMessage(String filename, ActorRef collectionActor) {
+        this.filename = filename;
+        this.collectionActor = collectionActor;
+    }
 }
